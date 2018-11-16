70f083cfec8dbea36a715dfc7990a51d15ab06ef
val <val@kreit.ru>
2018-11-16 17:13:06 +0500

    ftr: что-то все-таки успел

--- in0k_LazarusIdeEXT__wndSatellite_ObjectInspector4FormDesigner.pas
+++ in0k_LazarusIdeEXT__wndSatellite_ObjectInspector4FormDesigner.pas
@@ -4,6 +4,7 @@
 
 unit in0k_LazarusIdeEXT__wndSatellite_ObjectInspector4FormDesigner;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
@@ -13,7 +14,9 @@ uses
   in0k_lazarusIdeSRC__fuckUp_onActivate, 
   in0k_lazarusIdeSRC__ideForm_ObjectInspector, in0k_lazarusIdeSRC__B2SP, 
   in0k_bringToSecondPlane_LazLCL, in0k_bringToSecondPlane_WinAPI, 
-  in0k_lazarusIdeSRC__wndSatellite_templates__4FormDesigner, LazarusPackageIntf;
+  in0k_lazarusIdeSRC__wndSatellite_templates__4FormDesigner, 
+  in0k_lazarusIdeSRC__tControl_fuckUpWndProc, 
+  in0k_lazarusIdeSRC__tControls_fuckUpWndProc, LazarusPackageIntf;
 
 implementation
 
